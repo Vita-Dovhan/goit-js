f77acc1696bd17c7b86f2430cfbc43fe9f83d9ff
Vita Dovhan <87687287+Vita-Dovhan@users.noreply.github.com>
2021-10-24 23:06:38 +0300

    variables

---
+++ index.js
@@ -0,0 +1,34 @@
+//const age = 10;
+//const message = "Hello!";
+//const isOpen = true;
+//const type = typeof `kjhhhh`;
+//const m = "before";
+//const n = "after";
+//const shouldRenew = confirm("Чи бажаєте продовжити підписку?")
+//let Chyslo = prompt("К-ть товару");
+//Chyslo = Number(Chyslo);
+//let a = 15.67;
+//console.log(a);
+//a = a.toFixed(1);
+//a = Number(a);
+//console.log(a)
+//або
+//a = Number(a.toFixed(1));
+// i
+//console.log(Number(a.toFixed(1)))
+const base = prompt('Введіть основу, будь ласка');
+const power = prompt('Введіть степінь, будь ласка');
+const result = base ** power;
+console.log(result);
+
+
+//console.log(prompt);
+//console.log(Chyslo);
+//console.log(shouldRenew)
+//console.log(age);
+//console.log(message);
+//console.log(isOpen);
+//console.log(typeof Chyslo);
+//console.log(m);
+//alert("kjkkkjk");
+//console.log(n);
